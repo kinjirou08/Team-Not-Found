04b7fbe93f27d1fa7052fc2e2a422ee8c6c1b733
kinjirou08 <89804589+kinjirou08@users.noreply.github.com>
2021-12-14 15:13:39 -0600

    added spring boot configuration

---
+++ case-clothes/src/main/java/com/revature/caseclothes/CaseClothesApplication.java
@@ -0,0 +1,13 @@
+package com.revature.caseclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseClothesApplication.class, args);
+	}
+
+}
---
+++ case-clothes/src/test/java/com/revature/caseclothes/CaseClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.caseclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
